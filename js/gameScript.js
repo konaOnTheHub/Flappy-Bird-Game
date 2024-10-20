cfc3c321855b4c501a499583fff52874bd413850
konaOnTheHub <nagy.petike2@gmail.com>
2024-10-20 19:00:59 +0100

    Added gravity

--- js/gameScript.js
+++ js/gameScript.js
@@ -1,6 +1,21 @@
 var tubeTop = document.getElementsByClassName("tubeTop")[0];
+var bird = document.getElementsByClassName("bird")[0];
 
 tubeTop.addEventListener("animationiteration", () => {
     var ranNum = Math.random() * (550 - 150) + 150;
     tubeTop.style.height = ranNum + "px";
 });
+
+//gravity
+var gravity = setInterval(function () {
+    var x = getComputedStyle(bird);
+    var marginVal = parseInt(x.marginTop);
+    marginVal = marginVal + 3;
+    if (marginVal >= 750) {
+        alert("ur dead");
+        clearInterval(gravity);
+    };
+    bird.style.marginTop = marginVal + "px"
+
+}, 10)
+
