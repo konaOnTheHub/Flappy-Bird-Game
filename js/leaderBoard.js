f4558fca21fc9975950a7e804480fa94778a8bab
konaOnTheHub <nagy.petike2@gmail.com>
2024-10-25 17:13:35 +0100

    Styled leaderboard

--- js/leaderBoard.js
+++ js/leaderBoard.js
@@ -16,16 +16,23 @@ function sortPlayers() {
 };
 let players = sortPlayers()
 
+function userPos() {
+    document.getElementById("h2LeaderboardText").innerText = "Your current rank is: " + (players.findIndex(x => x.name === checkIfLoggedIn()) + 1);
+    
 
+}
 
 const newTable = document.createElement("table");
-newTable.innerHTML = "<thead><th>Player</th><th>Score</th></thead>";
+newTable.innerHTML = "<thead><th>Rank</th><th>Player</th><th>Highscore</th></thead>";
 for(player of players){
     const newRow = document.createElement("tr");
+    const tdRank = document.createElement("td");
     const tdPlayer = document.createElement("td");
     const tdScore = document.createElement("td");
+    tdRank.textContent = (players.indexOf(player)) + 1;
     tdPlayer.textContent = player.name;
     tdScore.textContent = player.score;    
+    newRow.appendChild(tdRank);
     newRow.appendChild(tdPlayer);
     newRow.appendChild(tdScore);
     newTable.appendChild(newRow);
